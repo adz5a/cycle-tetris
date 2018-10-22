9e76a16b1ea172439db6b5d23dc92691d34eeeaf
adz5a <antoine.d.zimmermann@gmail.com>
2018-10-22 23:12:36 +0200

    fixup! Step 0

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
-import xs from "xstream";
+import xs, { Stream } from "xstream";
 import { run } from "@cycle/run";
 import {
     makeDOMDriver,
-    div
+    DOMSource,
+    div,
+    VNode
 } from "@cycle/dom";
 
 
-const root = document.getElementById("app");
+const root = document.getElementById("app") as any as HTMLElement;
 
-const main = () => {
+interface ISource {
+    DOM: DOMSource;
+}
+
+interface ISink {
+    DOM: Stream<VNode>;
+}
+
+const main = (sources: ISource): ISink => {
     
-    const DOM = xs.of(div(["hello word, how are you ?"]));
+    const DOM = xs.of(div(["hello word, how are you ?"])) as Stream<VNode>;
 
     return { DOM };
 };
