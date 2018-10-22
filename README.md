c28b568f35f8f68d56e2ab340352076c06817a16
adz5a <antoine.d.zimmermann@gmail.com>
2018-10-22 23:59:42 +0200

    Step 2 - Refactor using @cycle/state

--- README.md
+++ README.md
@@ -1,36 +1,15 @@
 # README
 
-## Step 1 - A basic reactive application
+## Step 2 - Use @cycle/state
 
-The goal here is to discover how to use the tools to:
-
-- handle user input
-- use user input to act on the state of the DOM
-
-### Handle user input
-
-We will simply display a button which can be clicked.
-
-CycleJS revolves around the idea of reactive programming and uses *streams* as
-its main primitive to express any applicative logic.
-
-In order to display anything in the DOM, it needs to be made available to the
-framework through a *value* returned by a *stream* to a *driver*.
-
-In our case the value vill be a *virtual node* compatible with `snabbdom`[1],
-the stream will be the value returned by the `main` function as the `DOM` key
-and the driver will be the official `@cycle/dom` driver which handles DOM
-updates for us.
+The goal is to refactor our app to use `@cycle/state`
 
 
-The scenario is as follow: 
+Using this tool, we can express the different transformation of our app in a
+very expressive way:
 
-- The DOM displays a counter and a button
-- A ticker emits an event every second, when received this ticker increases the
-  count.
-- When the button is clicked, the ticker is paused. When clicked again it
-  resumes.
+- increment the counter
+- toggle the status of the app
 
-[1] Snabbdom is a virtual dom library which allows us to model the DOM as a
-value (namely virtual nodes) and express the DOM mutation as a patch operation
-on these virtual nodes.
+
+Those operations are mapped to periodic signals or user interactions.
--- src/index.ts
+++ src/index.ts
@@ -8,45 +8,79 @@ import {
     button,
     span
 } from "@cycle/dom";
+import {
+    withState,
+    StateSource,
+    Reducer
+} from "@cycle/state";
 
 
 const root = document.getElementById("app") as any as HTMLElement;
 
+type Status = "paused" | "active";
+
+interface State<S extends Status> {
+    status: S;
+    count: number;
+}
+
 interface ISource {
     DOM: DOMSource;
+    state: StateSource<State<Status>>;
 }
 
 interface ISink {
     DOM: Stream<VNode>;
+    state: Stream<Reducer<State<Status>>>;
 }
+
+const defaultState: State<"paused"> = {
+    status: "paused",
+    count: 0
+};
+
+const increaseCounter = (state: State<Status> = defaultState): State<Status> => {
+    return {
+        ...state,
+        count: state.count + 1,
+    };
+};
+
+const toggleState = (state: State<Status> = defaultState): State<Status> => {
+
+    if (state.status === "active") {
+        return {
+            ...state,
+            status: "paused"
+        };
+    }
+    return {
+        ...state,
+        status: "active"
+    };
+};
 
 const main = (sources: ISource): ISink => {
 
     const click$ = sources.DOM.select(".inc")
         .events("click");
 
-    const state$ = click$
-        .fold((state, _) => {
+    const status$ = sources.state.stream
+        .map((state) => state.status);
 
-            if (state.status === "active") {
-                return {
-                    status: "paused",
-                    $: xs.never()
-                };
+    const count$ = sources.state.stream
+        .map((state) => state.count);
+
+    const tick$: Stream<any> = status$
+        .fold((stream, status) => {
+
+            if (status === "paused") {
+                return xs.never();
             }
-            return {
-                status: "active",
-                $: xs.periodic(1000)
-            };
+            return xs.periodic(1000);
 
-        }, {
-            status: "active",
-            $: xs.periodic(1000)
-        });
-
-    const tick$ = state$.map(state => state.$).flatten();
-    const count$ = tick$.fold((count, _) => count + 1, 0);
-    const status$ = state$.map(state => state.status);
+        }, xs.never())
+        .flatten();
 
     const counter$ = count$
         .map((count) => {
@@ -60,6 +94,10 @@ const main = (sources: ISource): ISink => {
             ]);
         });
 
+    const inc$ = tick$.mapTo(increaseCounter);
+
+    const toggleStatus$ = click$.mapTo(toggleState);
+
     const button$ = status$.map((status) => button(".inc", [status]));
     const dom$ = xs
         .combine(counter$, button$)
@@ -68,10 +106,15 @@ const main = (sources: ISource): ISink => {
         });
 
     return {
-        DOM: dom$
+        DOM: dom$,
+        state: xs.merge(
+            xs.of(toggleState), // will start the app
+            inc$,
+            toggleStatus$
+        )
     };
 };
 
-run(main, {
+run(withState(main), {
     DOM: makeDOMDriver(root)
 });
